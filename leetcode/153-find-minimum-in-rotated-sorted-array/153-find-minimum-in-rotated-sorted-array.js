a45437dba418cfba20e74ff11c8d1c9e736e6461
bidhannath <bidhannath43@gmail.com>
2026-02-22 17:11:20 +0530

    feat: add js solution for 153-find-minimum-in-rotated-sorted-array

---
+++ leetcode/153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+  let left = 0, right = nums.length - 1;
+  let mid;
+  while (left < right) {
+    mid = (left + right) >> 1;
+    if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return nums[left];
+};
